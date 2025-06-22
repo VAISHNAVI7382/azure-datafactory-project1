dca858c62209f6329d7ca33fc65c57cb1ed127ca
VAISHNAVI GOPALUNI <42315342+VAISHNAVI7382@users.noreply.github.com>
2025-06-22 16:33:36 -0400

    Update bronze_notebook.py

--- bronze_notebook.py
+++ bronze_notebook.py
@@ -1,4 +1,4 @@
 # Bronze Layer - Ingest JSON from ADLS
 from pyspark.sql.functions import col, to_date, lower
-df_bronze = spark.read.parquet("https://vaishnaviadlsgen2june.blob.core.windows.net/datafiles/bronze/VAISHNAVI7382/azure-datafactory-project1/refs/heads/main/")
+df_bronze = spark.read.parquet("https://vaishnaviadlsgen2june.blob.core.windows.net/datafiles/bronze/VAISHNAVI7382/azure-datafactory-project1/refs/heads/main/retail_transactions_bronze.parquet")
 df_bronze.show()
